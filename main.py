e99a650645019d6802da4f299cf541abd7c0ab0e
PotatoK123 <56174807+PotatoK123@users.noreply.github.com>
2019-10-04 23:35:33 +0100

    Added a fail-safe
    Added a fail-safe and a better output for the user.

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ import random
 number = random.randint(1,10)
 tries = 3
 
-guess = input("I have a number between 1 and 10. Can you guess what it is? You have 3 guesses! ")
-
-answer = int(guess)
+print("I have a number between 1 and 10. Can you guess what it is?")
 
 while tries > 0:
-  if answer == number:
+
+  while True:
+    try:
+      guess = int(input("The amount of guesses you have is " + str(tries) + "\n"))
+      break
+
+    except:
+      print("Please enter a number!")
+  
+  if guess == number:
     print("You win!")
     break
+  
   else:
     tries -= 1
+    
     if tries == 0:
       print("Sorry! You lose")
       break
-    else:
-      input("Try again ")
-    
+      
